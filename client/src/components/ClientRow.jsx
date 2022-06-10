4e821e461a5ddbcd65e7d05787402f3590093486
Gerry Bong <ggbong734@gmail.com>
2022-06-09 20:44:56 -0700

    completed updating delete client so it deletes project too

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,21 +2,23 @@ import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/clientMutations";
 import { GET_CLIENTS } from "../queries/clientQueries";
+import { GET_PROJECTS } from "../queries/projectQueries";
 
 export default function ClientRow({ client }) {
   // apollo is like our state manager, can bring any query into any component
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    // refetchQueries: [{ query: GET_CLIENTS }],
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    // use refetch instead of update so we can update both projects and clients when we delete a client
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
+    // update(cache, { data: { deleteClient } }) {
+    //   const { clients } = cache.readQuery({ query: GET_CLIENTS });
+    //   cache.writeQuery({
+    //     query: GET_CLIENTS,
+    //     data: {
+    //       clients: clients.filter((client) => client.id !== deleteClient.id),
+    //     },
+    //   });
+    // },
   });
 
   return (
--- client/src/components/EditProjectForm.jsx
+++ client/src/components/EditProjectForm.jsx
@@ -1,16 +1,32 @@
 import { useMutation } from "@apollo/client";
 import { useState } from "react";
 import { GET_PROJECT } from "../queries/projectQueries";
+import { UPDATE_PROJECT } from "../mutations/projectMutations";
 
 export default function EditProjectForm({ project }) {
   const [name, setName] = useState(project.status);
   const [description, setDescription] = useState(project.description);
   const [status, setStatus] = useState("");
 
+  const [updateProject] = useMutation(UPDATE_PROJECT, {
+    variables: { id: project.id, name, description, status },
+    refetchQueries: [{ query: GET_PROJECT, variables: { id: project.id } }],
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !description || !status) {
+      return alert("Please fill out all fields");
+    }
+
+    updateProject(name, description, status);
+  };
+
   return (
     <div className="mt-5">
       <h3>Update Project Details</h3>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="mb-3">
           <label className="form-label">Name</label>
           <input
@@ -43,6 +59,9 @@ export default function EditProjectForm({ project }) {
             <option value="completed">Completed</option>
           </select>
         </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
       EditProjectForm
     </div>
